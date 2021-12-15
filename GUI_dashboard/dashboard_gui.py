cf19a3c094210891bacd09b892cae1badc8eda95
geertgeert <geertvdwilt@gmail.com>
2021-12-15 20:54:38 +0100

    Create dashboard_gui.py

---
+++ GUI_dashboard/dashboard_gui.py
@@ -0,0 +1,115 @@
+from pathlib import Path
+from tkinter import *
+
+
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("./assets")
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+window = Tk()
+window.title('Steam')
+window.geometry("1280x720")
+window.configure(bg = "#FFFFFF")
+
+canvas = Canvas(
+    window,
+    bg = "#FFFFFF",
+    height = 720,
+    width = 1280,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+
+# def pop_up(timer, message):
+#     pop_up = Toplevel(window)
+#     pop_up.geometry("650x70")
+#     pop_up.configure(bg = "#fcc63f")
+#     pop_up.resizable(False, False)
+#     Label(pop_up, 
+#     text=message,
+#     fg = "#212b5c",
+#     bg = "#fcc63f",
+#     font = "Arial 26 bold").pack(pady=10)
+#     pop_up.after(timer, lambda: pop_up.destroy())
+
+
+# image_image_3 = PhotoImage(
+#     file=relative_to_assets("image_3.png"))
+# image_3 = canvas.create_image(
+#     240.0,
+#     360.0,
+#     image=image_image_3
+# )
+
+# button_image_1 = PhotoImage(
+#     file=relative_to_assets("button_1.png"))
+
+naamtxt = Label(window, 
+    text="Naam",
+    fg = "#212b5c",
+    bg = "#fcc63f",
+    font = "Arial 48 bold",
+    anchor=E)
+naamtxt.place(
+    x=200.0,
+    y=70.0,
+    width=250,
+    height=60)
+
+# naamnote = Label(window, 
+#     text="(optioneel)",
+#     fg = "#212b5c",
+#     bg = "#fcc63f",
+#     font = "Arial 20 bold",
+#     anchor=E)
+# naamnote.place(
+#     x=198.0,
+#     y=120.0,
+#     width=250,
+#     height=24)
+
+berichttxt = Label(window, 
+    text="Bericht",
+    fg = "#212b5c",
+    bg = "#fcc63f",
+    font = "Arial 48 bold",
+    anchor=E)
+berichttxt.place(
+    x=200.0,
+    y=180.0,
+    width=250,
+    height=60)
+
+# input_name = Text(
+#     fg = "#212b5c",
+#     bg = "white",
+#     font = "Arial 36",
+#     insertbackground="#212b5c")
+# input_name.place(
+#     x=530.0,
+#     y=70.0,
+#     width=700.0,
+#     height=58.0
+# )
+
+# input_message = Text(
+#     fg = "#212b5c",
+#     bg = "white",
+#     font = "Arial 24",
+#     wrap='word',
+#     insertbackground="#212b5c"
+# )
+# input_message.place(
+#     x=530.0,
+#     y=180.0,
+#     width=600.0,
+#     height=490.0
+# )
+
+gui.window.mainloop()
