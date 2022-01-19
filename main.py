0aef627cd01bb6beaf2131a93dbf861f57c6397b
geertgeert <geertvdwilt@gmail.com>
2022-01-19 14:57:05 +0100

    Random

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from GUI_dashboard import dashboard_gui
 import json, threading, time
 import ai
+import random
+
+
+random.seed()	
+game = random.randrange(0,300)
+print(game)
 
 def start_gui():
         data = json.load(open("steam.json"))
         print(data[0])
-        dashboard_gui.data_appid.config(text=data[0]['appid'])
-        dashboard_gui.data_name.config(text=data[0]['name'])
-        dashboard_gui.data_platforms.config(text=data[0]['platforms'])
-        dashboard_gui.data_required_age.config(text=data[0]['required_age'])
+        dashboard_gui.data_appid.config(text=data[game]['appid'])
+        dashboard_gui.data_name.config(text=data[game]['name'])
+        dashboard_gui.data_platforms.config(text=data[game]['platforms'])
+        dashboard_gui.data_required_age.config(text=data[game]['required_age'])
         #dashboard_gui.data_most_positive_ratings.config(text=ai.most_positive_reviews())
         #dashboard_gui.data_review_ratio.config(text=ai.review_ratio())
 
