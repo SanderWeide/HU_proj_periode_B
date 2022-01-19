9f63955cef0527c8fe2abbc03772b68ffbc054fe
geertgeert <geertvdwilt@gmail.com>
2022-01-19 15:05:51 +0100

    Aanpassing main

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def start_gui():
         dashboard_gui.data_name.config(text=data[game]['name'])
         dashboard_gui.data_platforms.config(text=data[game]['platforms'])
         dashboard_gui.data_required_age.config(text=data[game]['required_age'])
-        #dashboard_gui.data_most_positive_ratings.config(text=ai.most_positive_reviews())
-        #dashboard_gui.data_review_ratio.config(text=ai.review_ratio())
+        dashboard_gui.data_most_positive_ratings.config(text=ai.most_positive_reviews())
+        dashboard_gui.data_review_ratio.config(text=ai.review_ratio())
 
 start_gui()
 
